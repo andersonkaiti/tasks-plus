6be2f90f429ed80eadb3da60683ae52ec9c8c642
andersonkaiti <anderkaiti@gmail.com>
2025-11-19 14:15:16 -0300

    :sparkles: feat(web,api-client): add token-based authorization to api client requests

--- web/src/http/api-client.ts
+++ web/src/http/api-client.ts
@@ -1,6 +1,18 @@
 import { env } from '@config/env'
+import Cookies from 'js-cookie'
 import ky from 'ky'
 
 export const api = ky.create({
   prefixUrl: env.VITE_PUBLIC_API_URL,
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        const token = Cookies.get('token')
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
 })
