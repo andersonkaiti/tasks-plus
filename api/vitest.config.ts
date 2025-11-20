29ae0ba12145e3546fc2c58463172757673e1140
andersonkaiti <anderkaiti@gmail.com>
2025-11-20 14:14:18 -0300

    :test_tube: ci(api): add github actions workflow for running tests and coverage reporting

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['./**/*.{e2e,test}.ts'],
+    coverage: {
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    },
   },
 })
