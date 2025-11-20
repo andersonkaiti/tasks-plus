d3699fd9e1ae676545a414c484565c14731f8167
andersonkaiti <anderkaiti@gmail.com>
2025-11-20 14:51:13 -0300

    :test_tube: ci(api): remove test coverage report and fix pnpm-lock path

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['./**/*.{e2e,test}.ts'],
-    coverage: {
-      reporter: ['text', 'json-summary', 'json'],
-      reportOnFailure: true,
-    },
   },
 })
