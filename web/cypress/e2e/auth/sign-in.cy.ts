2c97290bfc1479bc577c376529e4ad141cd1e77b
andersonkaiti <anderkaiti@gmail.com>
2025-11-21 19:01:13 -0300

    :sparkles: feat(web,test): add end-to-end tests for sign-in flow

---
+++ web/cypress/e2e/auth/sign-in.cy.ts
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker'
+
+describe('should be able to sign up', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('it should be able to navigate to the sign in page and authenticate', () => {
+    cy.contains('Criar conta').click()
+
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+
+    cy.get('input[name="username"]').type(faker.person.fullName())
+    cy.get('input[name="email"]').type(email)
+    cy.get('input[name="password"]').type(password)
+    cy.get('input[name="confirmPassword"]').type(password)
+
+    cy.contains('Criar conta').click()
+
+    cy.get('input[name="email"]').type(email)
+    cy.get('input[name="password"]').type(password)
+
+    cy.contains('Entrar').click()
+
+    cy.contains('Login realizado com sucesso!').should('exist')
+  })
+})
